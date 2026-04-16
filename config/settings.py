30cc3cc18f9fd0ed746099a2e47b7b31b3a743f4
Basit Ali <basit610476@gmail.com>
2026-04-16 22:49:53 +0500

    fix: added database-url

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+        'default': dj_database_url.config(default=os.environ.get("postgresql://postgres:alVlGFZdkpnnKVekvTBhoFDjIyisFfUV@postgres.railway.internal:5432/railway"))
 
     }
 }
