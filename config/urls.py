ea144932f98347491968be6cc459236c3f310fe2
ImBasit-ali <basit610476@gmail.com>
2026-05-05 14:07:57 +0500

    fix deploy error

--- config/urls.py
+++ config/urls.py
@@ -6,10 +6,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path
+from .views import worker_health_check
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('segmentation.urls')),
+    path('health/', worker_health_check),
+
 ]
 
 # Serve uploaded and generated media files for the viewer in all environments.
